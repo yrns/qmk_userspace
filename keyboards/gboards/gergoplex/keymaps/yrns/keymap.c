aa1b79a37680d069a8738e1a9858e196457f6439
Al McElrath <hello@yrns.org>
2024-05-05 15:08:40 -0700

    fixes for latest qmk

--- keyboards/gboards/gergoplex/keymaps/yrns/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/yrns/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-#include "g/keymap_combo.h"
+//#include "g/keymap_combo.h"
 
 // from https://docs.qmk.fm/#/faq_debug
 void keyboard_post_init_user(void) {
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
   // FIX: right thumbs unused?
   [FN] = LAYOUT_split_3x5_3(
-    RESET,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,
-    KC_NO,   KC_NO,   KC_NO,   TG(MODS), KC_NO,  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK,
-    DEBUG,   KC_ALGR, KC_NO,   KC_NO,    KC_NO,  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,
+    QK_BOOT, KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,
+    KC_NO,   KC_NO,   KC_NO,   TG(MODS), KC_NO,  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,
+    DB_TOGG, KC_ALGR, KC_NO,   KC_NO,    KC_NO,  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,
     /*             */ KC_APP,  KC_SPC,   KC_TAB, KC_NO,   KC_NO,   KC_NO
   ),
 };
