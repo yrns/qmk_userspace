632a712cc3f89e87165050cd40d008a143891114
Al McElrath <hello@yrns.org>
2024-05-16 14:10:29 -0700

    add some fn keys to the left side "number row"

--- keyboards/gboards/gergoplex/keymaps/yrns/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/yrns/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
   // FIX: right thumbs unused?
   [FN] = LAYOUT_split_3x5_3(
-    QK_BOOT, KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,
+    QK_BOOT, KC_F2,   KC_F3,   KC_F4,    KC_F5,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,
     KC_NO,   KC_NO,   KC_NO,   TG(MODS), KC_NO,  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,
     DB_TOGG, KC_ALGR, KC_NO,   KC_NO,    KC_NO,  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,
     /*             */ KC_APP,  KC_SPC,   KC_TAB, KC_NO,   KC_NO,   KC_NO
